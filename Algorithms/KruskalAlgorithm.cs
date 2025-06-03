cc4f940645cb20d6610909124cfc4a53aa313ff3
Тихенко Микола <132196834+Callibri@users.noreply.github.com>
2025-06-04 02:01:58 +0300

    Update KruskalAlgorithm.cs

--- Algorithms/KruskalAlgorithm.cs
+++ Algorithms/KruskalAlgorithm.cs
@@ -1,5 +1,4 @@
-﻿
-using GraphSolver.Entities;
+﻿using GraphSolver.Entities;
 using GraphSolver.GraphComponents;
 using System;
 using System.Collections.Generic;
@@ -8,12 +7,12 @@ using System.Linq;
 
 namespace GraphSolver.Algorithms
 {
-    public class KruskalAlgorithm : ISpanningTreeAlgorithm 
+    public class KruskalAlgorithm : ISpanningTreeAlgorithm
     {
-        public (List<Edge> spanningTree, double totalWeight, TimeSpan timeTaken) FindMST(Graph graph)
+        public (HashSet<Edge> spanningTree, double totalWeight, TimeSpan timeTaken) FindMST(Graph graph)
         {
             var stopwatch = Stopwatch.StartNew();
-            var mst = new List<Edge>();
+            var mst = new HashSet<Edge>();
             double totalWeight = 0;
 
             if (graph.Vertices.Count == 0)
@@ -27,14 +26,14 @@ namespace GraphSolver.Algorithms
 
             foreach (var edge in sortedEdges)
             {
-                var root1 = ds.Find(edge.Source);
-                var root2 = ds.Find(edge.Destination);
+                var root1 = ds.Find(edge.Source); 
+                var root2 = ds.Find(edge.Destination); 
 
                 if (!root1.Equals(root2))
                 {
-                    mst.Add(edge);
+                    mst.Add(edge); 
                     totalWeight += edge.Weight;
-                    ds.Union(edge.Source, edge.Destination);
+                    ds.Union(edge.Source, edge.Destination); 
 
                     if (mst.Count == graph.Vertices.Count - 1)
                         break;
@@ -45,23 +44,41 @@ namespace GraphSolver.Algorithms
             return (mst, totalWeight, stopwatch.Elapsed);
         }
 
-
         public string GetPracticalComplexityFormula(Graph graph)
         {
-            int V = graph.Vertices.Count; 
-            int E = graph.Edges.Count;    
+            int V = graph.Vertices.Count;
+            int E = graph.Edges.Count;
 
             if (V == 0) return "O(0) - порожній граф";
 
-            
-            return $"O(E log E) або O(E log V), де V = {V}, E = {E}";
+            bool areVerticesAlmostOnSameHeight = false;
+            if (V > 1)
+            {
+                var yCoordinates = graph.Vertices.Select(v => v.Y).ToList();
+                double minY = yCoordinates.Min();
+                double maxY = yCoordinates.Max();
+                double heightThreshold = 10.0; 
+
+                if ((maxY - minY) < heightThreshold)
+                {
+                    areVerticesAlmostOnSameHeight = true;
+                }
+            }
+
+            if (areVerticesAlmostOnSameHeight)
+            {
+                return $"O(E log* V) (E={E}, V={V}) - вершини майже на одній висоті";
+            }
+            else
+            {
+                return $"O(E log E) (E={E}, V={V})";
+            }
         }
 
 
         public string GetTheoreticalComplexityFormula()
         {
-            
-            return "O(E log E) або O(E log V)";
+            return "O(E log E)";
         }
     }
 }
